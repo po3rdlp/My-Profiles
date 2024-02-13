b19b2c2c3302148049401f41518e6e261eb44a76
po3rdlp <leonpolandos53@gmail.com>
2024-02-13 10:51:47 +0800

    Adding Navigation Bar mobile view, & pinia

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,7 +6,7 @@
           <div class="flex-1">
             <div class="flex w-10 h-10 rounded-full">
               <img
-                class="rounded-2xl"
+                class="rounded-3xl"
                 alt="Tailwind CSS Navbar component"
                 src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
               />
@@ -14,62 +14,78 @@
             </div>
           </div>
           <div class="flex items-center justify-end gap-3">
-            <label class="cursor-pointer grid place-items-center">
-              <input
-                type="checkbox"
-                value="synthwave"
-                class="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
-              />
-              <svg
-                class="col-start-1 row-start-1 stroke-base-100 fill-base-100"
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+            <div>
+              <button
+                @click="weatherCondition = !weatherCondition"
+                :class="{
+                  'animate__animated animate__fadeIn': weatherCondition,
+                }"
               >
-                <circle cx="12" cy="12" r="5" />
-                <path
-                  d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
-                />
-              </svg>
-              <svg
-                class="col-start-2 row-start-1 stroke-base-100 fill-base-100"
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path
-                  d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"
-                ></path>
-              </svg>
-            </label>
-            <button @click="openDrawer">
-              <Icon name="system-uicons:menu-hamburger" size="34px" />
-            </button>
+                <span v-if="isClearNight">
+                  <Icon name="meteocons:clear-night" size="34px" />
+                </span>
+                <span v-else-if="isClearDay">
+                  <Icon name="meteocons:clear-day" size="34px" />
+                </span>
+              </button>
+            </div>
+            <div>
+              <button @click="toggleDrawer">
+                <span :class="{ hidden: isDrawerOpen === true }">
+                  <Icon name="system-uicons:menu-hamburger" size="34px" />
+                </span>
+              </button>
+              <button @click="toggleDrawer">
+                <span :class="{ hidden: isDrawerOpen === false }">
+                  <Icon name="tabler:playlist-x" size="34px" />
+                </span>
+              </button>
+            </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="drawer">
+      <input
+        id="my-drawer"
+        type="checkbox"
+        class="drawer-toggle"
+        v-model="isDrawerOpen"
+      />
+      <div class="drawer-content"></div>
+      <div class="drawer-side">
+        <label
+          for="my-drawer"
+          aria-label="close sidebar"
+          class="drawer-overlay"
+        ></label>
+        <ul class="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          <li>
+            <span @click="isDrawerOpen = false"
+              ><NuxtLink to="/">/</NuxtLink></span
+            >
+          </li>
+          <li>
+            <span @click="isDrawerOpen = false"
+              ><NuxtLink to="/home">home</NuxtLink></span
+            >
+          </li>
+        </ul>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="tsx">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 
 const isDrawerOpen = ref(false);
+const weatherCondition = ref(false);
 
-const openDrawer = () => {
-  isDrawerOpen.value = true;
+const toggleDrawer = () => {
+  isDrawerOpen.value = !isDrawerOpen.value;
 };
+
+const isClearDay = computed(() => weatherCondition.value === true);
+const isClearNight = computed(() => weatherCondition.value === false);
 </script>

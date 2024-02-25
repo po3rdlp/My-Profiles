737823e2507d5ef2539aaad58d40c32074b528b0
po3rdlp <leonpolandos53@gmail.com>
2024-02-25 23:56:59 +0800

    fix failure

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,12 +10,12 @@
     <img
       class="w-32 h-32 rounded-3xl bg-blend-darken"
       alt="Tailwind CSS Navbar component"
-      src="../../astronaut.png"
+      src="../../assets/img/astronaut.png"
     />
     <img
       class="w-32 h-32 rounded-3xl bg-blend-darken transform scale-x-[-1]"
       alt="Tailwind CSS Navbar component"
-      src="../../astronaut.png"
+      src="../../assets/img/astronaut.png"
     />
   </div>
 </template>

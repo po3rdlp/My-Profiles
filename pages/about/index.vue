b75430042a365625cbd136cadd8f0ec984300a22
po3rdlp <leonpolandos53@gmail.com>
2024-03-15 12:06:00 +0800

    update about

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -12,25 +12,11 @@
     </div>
     <div class="text-md font-normal grid gap-5 mb-5">
       <h1>
-        Hello! I'm Leonard Petter, commonly known as Leon. My fervent interest
-        lies in the realm of software engineering, and I bring with me a month
-        of experience as a Server Administrator and Project Leader at RSUD DR.
-        SAMRATULANGI TONDANO. In this role, I've spearheaded the development of
-        essential systems, applications, and websites for the hospital. My
-        primary focus has been on honing my skills as a FrontEnd Developer, with
-        a current emphasis on TypeScript, JavaScript, and Dart (currently in
-        progress). Within the confines of my professional journey, I've
-        successfully completed several pivotal projects for my workplace and
-        have also taken on additional projects beyond my regular job
-        responsibilities.
-      </h1>
-      <h1>
-        Remaining steadfast in my commitment to professional growth, I make it a
-        priority to stay updated and continually adopt best practices in the
-        ever-evolving landscape of software engineering. This dedication extends
-        beyond the confines of my workplace, as I actively seek and embrace
-        opportunities to further expand my skill set and contribute meaningfully
-        to the dynamic field of technology.
+        Leonard Petter, aka Leon, is a tech enthusiast who's led projects as a
+        Server Admin and Project Leader at RSUD DR. SAMRATULANGI TONDANO. His
+        forte? FrontEnd Development, with a focus on TypeScript, JavaScript, and
+        Dart. Always hungry for growth, he's on a quest for new skills and
+        innovations in the tech realm.
       </h1>
     </div>
     <AboutSkillsView />

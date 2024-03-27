9b69287851802d94514c67681fea54becedb3af5
po3rdlp <leonpolandos53@gmail.com>
2024-03-27 13:52:56 +0800

    update contact

---
+++ components/contact/MessageMe.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mt-3">
+    <label class="label"> Or Send Me A Message !!</label>
+    <div class="grid gap-5 grid-cols-1">
+      <div class="grid grid-cols-2 gap-5">
+        <input
+          type="text"
+          class="input input-primary rounded-xl border-gray-800"
+          placeholder="Name*"
+        />
+        <input
+          type="email"
+          class="input input-primary rounded-xl border-gray-800"
+          placeholder="Email*"
+        />
+      </div>
+      <div>
+        <textarea
+          class="input rounded-xl border-slate-800 w-full h-40"
+          placeholder="Message*"
+        ></textarea>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="grid justify-center">
-    <h1 class="text text-center animate-pulse">UNDER DEVELOPMENT</h1>
-    <h1 class="text text-center">CONTACT</h1>
-  </div>
-  <div class="flex justify-between">
-    <img
-      class="w-32 h-32 rounded-3xl bg-blend-darken"
-      alt="Tailwind CSS Navbar component"
-      src="../../assets/img/astronaut.png"
-    />
-    <img
-      class="w-32 h-32 rounded-3xl bg-blend-darken transform scale-x-[-1]"
-      alt="Tailwind CSS Navbar component"
-      src="../../assets/img/astronaut.png"
-    />
+  <div>
+    <div>
+      <h1 class="text text-2xl font-bold capitalize">Contact</h1>
+      <br />
+      <div class="animate__animated animate__fadeInDown">
+        <h1 class="flex">
+          Feel free to get in touch and let's have a discussion about how we can
+          work together.
+        </h1>
+      </div>
+      <hr class="h-px my-3 bg-gray-200 border-0 dark:bg-gray-700" />
+    </div>
+    <div class="mt-5">
+      <label class="text text-lg font-bold capitalize"
+        >Find Me on Social Media</label
+      >
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 mt-3 gap-5">
+        <button
+          class="btn btn-md rounded-lg bg-[#4EB067] hover:bg-[#4EB067] hover:scale-105 duration-400"
+        >
+          <Icon name="mdi:email-outline" size="30" /> Email
+        </button>
+        <button
+          class="btn btn-md rounded-lg bg-[#0077B5] hover:bg-[#0077B5] hover:scale-105 duration-400"
+        >
+          <Icon name="mdi:linkedin" size="30" /> Linkedin
+        </button>
+        <button
+          class="btn btn-md rounded-lg bg-[#9F64B6] hover:bg-[#9F64B6] hover:scale-105 duration-400"
+        >
+          <Icon name="mdi:instagram" size="30" /> Instagram
+        </button>
+        <button
+          class="btn btn-md rounded-lg bg-[#1DA1F2] hover:bg-[#1DA1F2] hover:scale-105 duration-400"
+        >
+          <Icon name="mdi:twitter" size="30" /> Email
+        </button>
+        <button
+          class="btn btn-md rounded-lg bg-[#79817D] hover:bg-[#79817D] hover:scale-105 duration-400"
+        >
+          <Icon name="mdi:github" size="30" /> Github
+        </button>
+      </div>
+    </div>
+    <br />
+    <hr class="h-px my-3 bg-gray-200 border-0 dark:bg-gray-700" />
+    <ContactMessageMe />
   </div>
 </template>

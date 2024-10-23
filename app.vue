fd63c4b185b33b128d5586eff798f608cd9cb19c
po3rdlp <leonpolandos53@gmail.com>
2024-10-23 12:27:12 +0800

    fix: hydration mismatch fix

--- app.vue
+++ app.vue
@@ -1,19 +1,29 @@
 <template>
   <div>
-    <NuxtLayout :name="layoutName">
-      <NuxtPage />
-    </NuxtLayout>
+    <div
+      v-if="!layoutReady"
+      class="bg-slate-300 animate animate-pulse w-full h-screen flex justify-center items-center"
+    >
+      <p class="font-bold text-center">LOADING ...</p>
+    </div>
+    <div v-else>
+      <NuxtLayout :name="layoutName">
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import "animate.css";
-
+import { ref, onMounted, computed } from "vue";
 const store = useMyStore();
+const layoutReady = ref(false);
 
 const initializeStore = async () => {
   await store.initialize();
   await store.checkToken();
+  layoutReady.value = true;
 };
 
 const layoutName = computed(() => {
